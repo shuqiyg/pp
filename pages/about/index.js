4b1121d1260151560f75ccedbf17645ddb6cff63
sq <wweerrbb@gmail.com>
2023-10-06 14:49:11 -0400

    update on about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -120,22 +120,40 @@ const About = () => {
       <div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6'>
         {/* text */}
         <div className='flex-1 flex flex-col justify-center'>
-          <h2 className='h2'>
+          <motion.h2
+            variants={fadeIn('right', .2)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='h2'
+          >
             Captivating <span className='text-accent'>stories</span> birth magnificent designs.
-          </h2>
-          <p className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0'>
+          </motion.h2>
+          <motion.p 
+            variants={fadeIn('right', .2)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0'
+          >
             10 years ago..........fsafsa sadf sd
             a fsafsa f
-          </p>
+          </motion.p>
           {/* counters */}
-          <div>
+          <motion.div 
+            variants={fadeIn('right', .6)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-8'
+          >
             <div className='flex flex-1 xl:gap-x-6'>
               {/* experience */}
               <div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0'>
                 <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
                   <Countup start={0} end={10} duration={5}></Countup>+
                 </div>
-                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>
+                <div className='text-xs uppercase tracking-[1px] leading-[1.] max-w-[120px]'>
                   Years of Exp
                 </div>
               </div>
@@ -144,7 +162,7 @@ const About = () => {
                 <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
                   <Countup start={0} end={300} duration={5}></Countup>+
                 </div>
-                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>
+                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[120px]'>
                   Statisfied clients
                 </div>
               </div>
@@ -153,7 +171,7 @@ const About = () => {
                 <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
                   <Countup start={0} end={650} duration={5}></Countup>+
                 </div>
-                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>
+                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[120px]'>
                   Finished Projects
                 </div>
               </div>
@@ -162,15 +180,21 @@ const About = () => {
                 <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
                   <Countup start={0} end={8} duration={5}></Countup>+
                 </div>
-                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>
+                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[120px]'>
                   Winning Awards
                 </div>
               </div>
             </div>
-          </div>
+          </motion.div>
         </div>  
         {/* info */}
-        <div className='flex flex-col w-full xl:max-w-[48%] h-[480px]'>
+        <motion.div 
+          variants={fadeIn('left', .5)}
+          initial='hidden'
+          animate='show'
+          exit='hidden'
+          className='flex flex-col w-full xl:max-w-[48%] h-[480px]'
+        >
           <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
             {aboutData.map((item, itemIndex) => {
               return (
@@ -206,7 +230,7 @@ const About = () => {
               )
             })}
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   )
