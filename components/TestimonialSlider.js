22f542e6edf6a491a8191d3ebf18bec166a1e73f
sq <wweerrbb@gmail.com>
2023-10-10 16:25:29 -0400

    finsihed draft v 1.0.0

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -1,3 +1,22 @@
+
+
+//import swiper react components
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+// import swiper sytles
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+// import required modules
+import { Navigation, Pagination} from 'swiper'
+
+//icons 
+import { FaQuoteLeft } from 'react-icons/fa'
+
+//next image
+import Image from 'next/image';
+
 // testimonial data
 export const testimonialSliders = [
   {
@@ -23,49 +42,38 @@ export const testimonialSliders = [
   },
 ];
 
-//import swiper react components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// import swiper sytles
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/pagination';
-
-// import required modules
-import { Pagination} from 'swiper';
-
-//icons 
-import { FaQuoteLeft } from 'react-icons/fa'
-//next image
-import Image from 'next/image';
-
 const TestimonialSlider = () => {
   return (
     <Swiper 
+      navigation={true}
       spaceBetween={10}
       pagination={{
         clickable: true,
       }}
       freeMode={true}
-      modules={[Pagination]}
-      className="h-[280px] sm:h-[480px]" 
+      modules={[Navigation, Pagination]}
+      className="h-[480px]" 
     >
       {
         testimonialSliders.map((person, index)=> {
           return (
             <SwiperSlide key={index}>
-              <div>
+              <div className='flex flex-col items-center md:flex-row gap-x-8 h-full px-16'>
                 {/* avatar, name, position */}
-                <div>
-                  {/* avatar */}
-                  <div>avatar img</div>
-                  {/* name */}
-                  <div>name</div>
-                  {/* position */}
-                  <di>position</di>
+                <div className='w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0'>
+                  <div className='flex flex-col justify-center text-center'>
+                    {/* avatar */}
+                    <div className='mb-2 mx-auto'>
+                      <Image src={person.image} width={100} height={100} alt=''></Image>
+                    </div>
+                    {/* name */}
+                    <div className='text-lg'>{person.name}</div>
+                    {/* position */}
+                    <div className='test-[12px] uppercase font-extralight tracking-widest'>{person.position}</div>
+                  </div>
                 </div>
                 {/* quote & message */}
-                <div className='bg-yellow-500/10 flex01 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20'>
+                <div className='flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20'>
                   {/* quote & icon */}
                   <div className='mb-4'>
                     <FaQuoteLeft className='text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0'/>
